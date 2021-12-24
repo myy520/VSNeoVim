1a3127fd9820656c608fca9c12bfb4e2cf24fbea
sameet <wpf_sam@yahoo.com>
2021-12-24 20:22:19 +0330

    adding options.

---
+++ lua/SuperVim/Core/options.lua
@@ -0,0 +1,54 @@
+local OPTIONS = vim.opt
+local GLOBAL = vim.g
+
+local optionsconfig = {
+  backup = false,
+  clipboard = 'unnamedplus',
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0,
+  fileencoding = 'utf-8',
+  hlsearch = true,
+  ignorecase = true,
+  mouse = 'a',
+  pumheight = 10,
+  showmode = false,
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  timeoutlen = 100,
+  undofile = true,
+  updatetime = 300,
+  writebackup = false,
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  cursorline = true,
+  number = true,
+  relativenumber = false,
+  numberwidth = 4,
+  signcolumn = 'yes',
+  wrap = false,
+  scrolloff = 8,
+  sidescrolloff = 8,
+  background = 'dark',
+}
+local globalconfig = {
+--  colors_name = 'darkplus',
+  colors_name = 'onedark',
+  onedark_style = 'deep',
+--  colors_name = 'rose-pine',
+}
+
+for k, v in pairs(globalconfig) do
+  GLOBAL[k] = v
+end
+for k, v in pairs(optionsconfig) do
+  OPTIONS[k] = v
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
+vim.cmd [[set formatoptions-=cro]]
