37ba053b9be78d4f808e9ee139c2750df8cd3fd5
sameet <wpf_sam@yahoo.com>
2022-02-03 22:19:15 +0330

    updating whichkey icons

--- lua/VisualStudioNeovim/Configs/WhichKey/init.lua
+++ lua/VisualStudioNeovim/Configs/WhichKey/init.lua
@@ -34,8 +34,8 @@ local setup = {
     -- ["<tab>"] = "TAB",
   },
   icons = {
-    breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-    separator = "➜", -- symbol used between a key and it's label
+    breadcrumb = ">", -- symbol used in the command line area that shows your active key combo
+    separator = "->", -- symbol used between a key and it's label
     group = "+", -- symbol prepended to a group
   },
   popup_mappings = {
