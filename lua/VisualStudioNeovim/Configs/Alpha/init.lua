bea457c6c5f1c910e48e9a8d6cbdb27ba4c8d44b
sameet <sameetmoosavi@yahoo.com>
2022-05-04 08:06:04 +0430

    update alpha footer

--- lua/VisualStudioNeovim/Configs/Alpha/init.lua
+++ lua/VisualStudioNeovim/Configs/Alpha/init.lua
@@ -37,6 +37,7 @@ dashboard.section.buttons.val = {
 	dashboard.button("SPC c", "  Configuration", ":NvimTreeOpen ~/.config/nvim/lua/VisualStudioNeovim/<CR>"),
 }
 
+dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
 
