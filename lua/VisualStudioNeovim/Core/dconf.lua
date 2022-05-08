72e59e7ce3b482530c7836f8df731f3db11f2ef7
sameet <sameetmoosavi@yahoo.com>
2022-05-08 17:48:37 +0430

    update dconf file

--- lua/VisualStudioNeovim/Core/dconf.lua
+++ lua/VisualStudioNeovim/Core/dconf.lua
@@ -47,5 +47,6 @@ vsn = {
   configs = {
     TreeIcons = {enabled = true},
     FileExplorer = {enabled = true, width = 30, position = "left", icons = true, diagnostics = true, open_file_on_new_tab = false, enable_git = true, hidden_ignore_files = true},
+    BufferLine = {enabled = true},
   }
 }
