aedfe1340d8c06a32b7528cceaa7f5a15629782f
sameet <sameetmoosavi@yahoo.com>
2022-05-17 23:04:04 +0430

    update lsp Diagnostics

--- lua/VisualStudioNeovim/Configs/LSP/init.lua
+++ lua/VisualStudioNeovim/Configs/LSP/init.lua
@@ -6,3 +6,15 @@ end
 require "VisualStudioNeovim.Configs.LSP.lsp-installer"
 require("VisualStudioNeovim.Configs.LSP.handlers").setup()
 require "VisualStudioNeovim.Configs.LSP.null-ls"
+
+vim.cmd [[
+  highlight! DiagnosticLineNrError guifg=#ec5f67 gui=bold
+  highlight! DiagnosticLineNrWarn  guifg=#ECBE7B gui=bold
+  highlight! DiagnosticLineNrInfo  guifg=#51afef gui=bold
+  highlight! DiagnosticLineNrHint  guifg=#98be65 gui=bold
+
+  sign define DiagnosticSignError text=  texthl=DiagnosticSignError linehl= numhl=DiagnosticLineNrError
+  sign define DiagnosticSignWarn  text=  texthl=DiagnosticSignWarn  linehl= numhl=DiagnosticLineNrWarn
+  sign define DiagnosticSignInfo  text=  texthl=DiagnosticSignInfo  linehl= numhl=DiagnosticLineNrInfo
+  sign define DiagnosticSignHint  text=  texthl=DiagnosticSignHint  linehl= numhl=DiagnosticLineNrHint
+]]
