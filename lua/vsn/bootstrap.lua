0efe4e6075113061c3fd669ecef27333a44e4605
sameet <sameetmoosavi@yahoo.com>
2022-06-16 16:27:06 +0430

    add get_data_home function and update get_runtime_dir function and

--- lua/vsn/bootstrap.lua
+++ lua/vsn/bootstrap.lua
@@ -10,11 +10,16 @@ if vim.fn.has "nvim-0.7" ~= 1 then
   vim.cmd "cquit"
 end
 
-function _G.get_runtime_dir()
-  local vsn_runtime_dir = os.getenv "VSN_RUNTIME_DIR"
-  if not vsn_runtime_dir then
+function _G.get_data_home()
+  local vsn_data_home = os.getenv "XDG_DATA_HOME"
+  if not vsn_data_home then
     return vim.call("stdpath", "data")
   end
+  return vsn_data_home
+end
+
+function _G.get_runtime_dir()
+  local vsn_runtime_dir = os.getenv "VSN_RUNTIME_DIR"
   return vsn_runtime_dir
 end
 
@@ -39,7 +44,16 @@ function C:init()
   self.config_dir = get_config_dir()
   self.cache_dir = get_cache_dir()
 
-
+  vim.fn.stdpath = function(what)
+    if what == "data" then
+      return _G.get_data_home() .. "/vsneovim-data"
+    elseif what == "config" then
+      return _G.get_config_dir()
+    elseif what == "cache" then
+      return _G.get_cache_dir()
+    end
+    return vim.call("stdpath", what)
+  end
 
   require("vsn.dconf.settings").load_options()
   require("vsn.shortcuts")
