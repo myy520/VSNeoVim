016cc5f24e468f92c4e40809aa3fe8db94dd1b0a
sameet <sameetmoosavi@yahoo.com>
2022-06-16 16:07:25 +0430

    add get_runtime_dir function

--- lua/vsn/bootstrap.lua
+++ lua/vsn/bootstrap.lua
@@ -10,6 +10,14 @@ if vim.fn.has "nvim-0.7" ~= 1 then
   vim.cmd "cquit"
 end
 
+function _G.get_runtime_dir()
+  local vsn_runtime_dir = os.getenv "VSN_RUNTIME_DIR"
+  if not vsn_runtime_dir then
+    return vim.call("stdpath", "data")
+  end
+  return vsn_runtime_dir
+end
+
 function C:init()
   require("vsn.dconf.settings").load_options()
   require("vsn.shortcuts")
