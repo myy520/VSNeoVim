d79d929ad80e2981455d236c820411edb09d7e03
sameet <sameetmoosavi@yahoo.com>
2022-06-16 15:49:23 +0430

    require utils in bootstrap

--- lua/vsn/bootstrap.lua
+++ lua/vsn/bootstrap.lua
@@ -1,5 +1,7 @@
 local C = {}
 
+local utils = require("vsn.utils")
+
 if vim.fn.has "nvim-0.7" ~= 1 then
   vim.notify("Please upgrade your Neovim base installation. VSNeoVim requires v0.7+", vim.log.levels.WARN)
   vim.wait(4000, function()
