e9751d9c13c077816f4c039030a32f17e2e48692
sameet <sameetmoosavi@yahoo.com>
2022-06-18 20:55:40 +0430

    add extension-loader.lua file

--- lua/vsn/extension-loader.lua
+++ lua/vsn/extension-loader.lua
@@ -1,7 +1,55 @@
-local extension_loader = {}
+local extesion_loader = {}
 
 local utils = require("vsn.utils")
 
-local compile_path = utils.join_paths(get_config_dir(), "plugin", "packer_compiled.lua")
+function extesion_loader:init()
+  local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    packer_bootstrap = vim.fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
+    vim.cmd "packadd packer.nvim"
+  end
 
-return extension_loader
+  local packer = utils.require_clean("packer")
+
+  packer.init {
+    plugin_package = "packer", -- The default package for plugins
+    auto_clean = true, -- During sync(), remove unused plugins
+    compile_on_sync = true, -- During sync(), run packer.compile()
+    disable_commands = false, -- Disable creating commands
+    opt_default = false, -- Default to using opt (as opposed to start) plugins
+    auto_reload_compiled = true, -- Automatically reload the compiled file after creating it.
+    display = {
+      non_interactive = false, -- If true, disable display windows for all operations
+      open_fn = function()
+        return require("packer.util").float { border = "rounded" }
+      end,
+      open_cmd = "65vnew \\[packer\\]", -- An optional command to open a window for packer"s display
+      working_sym = " ", -- The symbol for a plugin being installed/updated
+      error_sym = "", -- The symbol for a plugin with an error in installation/updating
+      done_sym = " ", -- The symbol for a plugin which has completed installation/updating
+      removed_sym = " ", -- The symbol for an unused plugin which was removed
+      moved_sym = "->", -- The symbol for a plugin which was moved (e.g. from opt to start)
+      header_sym = "━", -- The symbol for the header line in packer"s display
+      show_all_info = true, -- Should packer show all update details automatically?
+      prompt_border = "double", -- Border style of prompt popups.
+      keybindings = { -- Keybindings for the display window
+        quit = "q",
+        toggle_info = "<CR>",
+        diff = "d",
+        prompt_revert = "r",
+      }
+    },
+    log = { level = "warn" }, -- The default print log level. One of: "trace", "debug", "info", "warn", "error", "fatal".
+  }
+
+  return packer.startup(function(use)
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+      packer.sync()
+    end
+  end)
+
+end
+
+return extesion_loader
