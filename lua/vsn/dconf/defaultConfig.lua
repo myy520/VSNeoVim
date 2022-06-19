ef17ec82d8816bd440339be65c9072b32ba69b54
sameet <sameetmoosavi@yahoo.com>
2022-06-19 16:25:28 +0430

    add defaultConfig.lua

---
+++ lua/vsn/dconf/defaultConfig.lua
@@ -0,0 +1,25 @@
+vsn = {
+  shortcuts = {
+    options = { noremap = true, silent = true },
+    -- vim.keymap.set(mode, key, work, options)
+  },
+  options = {
+    -- vim.opt.OPTION
+    backup = false,
+    number = true,
+    shiftwidth = 2,
+    tabstop = 2,
+  },
+  extensions = {
+    -- add your oter extensions with this template
+    -- { "url" } 
+    -- and write configiration to 'configs'
+  },
+  configs = {
+    -- add your oter configs
+    -- for example :
+    --   vim.g.OPTION
+    --   require("alpha")...
+    --   and etc ...
+  },
+}
