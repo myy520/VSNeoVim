a74e96f92a4e50c6cde2718a5afc45baef8e7f27
sameet <sameetmoosavi@yahoo.com>
2022-06-19 16:21:56 +0430

    update settings.lua file

--- lua/vsn/dconf/settings.lua
+++ lua/vsn/dconf/settings.lua
@@ -2,7 +2,7 @@ local C = {}
 
 function C.load_options()
   local default_options = {
-    backup = false, -- creates a backup file
+    backup = vsn.options.backup, -- creates a backup file
     clipboard = "unnamedplus", -- allows neovim to access the system clipboard
     cmdheight = 2, -- more space in the neovim command line for displaying messages
     colorcolumn = "99999", -- fixes indentline for now
@@ -31,10 +31,10 @@ function C.load_options()
     updatetime = 300, -- faster completion
     writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
     expandtab = true, -- convert tabs to spaces
-    shiftwidth = 2, -- the number of spaces inserted for each indentation
-    tabstop = 2, -- insert 2 spaces for a tab
+    shiftwidth = vsn.options.shiftwidth, -- the number of spaces inserted for each indentation
+    tabstop = vsn.options.tabstop, -- insert 2 spaces for a tab
     cursorline = true, -- highlight the current line
-    number = true, -- set numbered lines
+    number = vsn.options.number, -- set numbered lines
     relativenumber = false, -- set relative numbered lines
     numberwidth = 4, -- set number column width to 2 {default 4}
     signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
