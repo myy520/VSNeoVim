4252fdf06f872df6a33567fe42da6e688318b9b4
sameet <sameetmoosavi@yahoo.com>
2022-06-20 15:47:37 +0430

    add vsn/core dir and set alpha configs

---
+++ lua/vsn/core/alpha/init.lua
@@ -0,0 +1,64 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+	return
+end
+
+local dashboard = require("alpha.themes.dashboard")
+
+local header = {
+  "                      ########               ########                      ",
+  "                    ############           ############                    ",
+  "                   ##############         ##############                   ",
+  "                  ################ ##### ################                  ",
+  "                  #######################################                  ",
+  "                  ################       ################                  ",
+  "                   ##############         ##############                   ",
+  "                    ############           ############                    ",
+  "                      ########               ########                      ",
+  "                    ###                             ###                    ",
+  "                   ###                               ###                   ",
+  "                   ###                               ###                   ",
+  "                   ###                               ###                   ",
+  "                   ###                               ###                   ",
+  "                    ####                            ####                   ",
+  "                     ####                          ####                    ",
+  " db    db .d8888.   d8b   db d88888b  .d88b.  db    db d888888b .88b  d88. ",
+  " 88    88 88'  YP   888o  88 88'     .8P  Y8. 88    88   `88'   88'YbdP`88 ",
+  " Y8    8P `8bo.     88V8o 88 88ooooo 88    88 Y8    8P    88    88  88  88 ",
+  " `8b  d8'   `Y8b.   88 V8o88 88~~~~~ 88    88 `8b  d8'    88    88  88  88 ",
+  "  `8bd8'  db   8D   88  V888 88.     `8b  d8'  `8bd8'    .88.   88  88  88 ",
+  "    YP    `8888Y'   VP   V8P Y88888P  `Y88P'     YP    Y888888P YP  YP  YP ",
+}
+local buttons = {
+  dashboard.button("n", "  New File", ":ene <BAR> startinsert <>"),
+  dashboard.button("f", "  Find File", ":Telescope find_files <>"),
+  dashboard.button("p", "  Recent Projects", ":Telescope projects <>"),
+  dashboard.button("r", "  Recently used files", ":Telescope oldfiles <>"),
+  dashboard.button("t", "  Find Text", ":Telescope live_grep <>"),
+  dashboard.button("c", "  configuration", ":edit ~/.config/vsn/config.lua<CR>"),
+}
+local types = {
+  header = "Include",
+  buttons = "Keyword",
+  footer = "Type",
+}
+local footer = {
+  text = "v 2.0.0"
+}
+noautocmd = true
+
+dashboard.section.header.val = header
+
+dashboard.section.buttons.val = buttons
+
+dashboard.section.header.opts.hl = types.header
+
+dashboard.section.buttons.opts.hl = types.buttons
+
+dashboard.section.footer.opts.hl = types.footer
+
+dashboard.section.footer.val = footer.text
+
+dashboard.opts.opts.noautocmd = noautocmd
+
+alpha.setup(dashboard.opts)
