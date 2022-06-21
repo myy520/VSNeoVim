d7ff0a8aac849f6dbc78ed571f375ae84da7229a
sameet <sameetmoosavi@yahoo.com>
2022-06-21 18:31:59 +0430

    add treesitter configuration

---
+++ lua/vsn/core/treesitter.lua
@@ -0,0 +1,22 @@
+local utils = require("vsn.utils")
+
+local ts_config = utils.require_clean("nvim-treesitter.configs")
+
+ts_config.setup {
+  ensure_installed = "all",
+  sync_install = false,
+  ignore_install = { "jsonc", "fusion", "ocamllex" },
+  autopairs = {
+    enable = true,
+  },
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = { "" }, -- list of language that will be disabled
+    additional_vim_regex_highlighting = true,
+  },
+  indent = { enable = true, disable = { "yaml" } },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+}
