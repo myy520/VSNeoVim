b73c81a0f496e768065384bdd1f4d3a2010215b4
sameet <sameetmoosavi@yahoo.com>
2022-06-21 13:04:09 +0430

    add cmp configurations

---
+++ lua/vsn/core/cmp.lua
@@ -0,0 +1,115 @@
+local utils = require("vsn.utils")
+
+local cmp = utils.require_clean("cmp")
+
+local luasnip = utils.require_clean("luasnip")
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
+local check_backspace = function()
+  local col = vim.fn.col "." - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
+local kind_icons = {
+  Class = " ",
+  Color = " ",
+  Constant = "ﲀ ",
+  Constructor = " ",
+  Enum = "練",
+  EnumMember = " ",
+  Event = " ",
+  Field = " ",
+  File = "",
+  Folder = " ",
+  Function = " ",
+  Interface = "ﰮ ",
+  Keyword = " ",
+  Method = " ",
+  Module = " ",
+  Operator = "",
+  Property = " ",
+  Reference = " ",
+  Snippet = " ",
+  Struct = " ",
+  Text = " ",
+  TypeParameter = " ",
+  Unit = "塞",
+  Value = " ",
+  Variable = " ",
+}
+
+local source_names = {
+  nvim_lsp = "(LSP)",
+  emoji = "(Emoji)",
+  path = "(Path)",
+  calc = "(Calc)",
+  cmp_tabnine = "(Tabnine)",
+  vsnip = "(Snippet)",
+  luasnip = "(Snippet)",
+  buffer = "(Buffer)",
+  tmux = "(TMUX)",
+}
+
+local config = {
+  confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  },
+  completion = {
+    keyword_length = 1,
+  },
+  experimental = {
+    ghost_text = true,
+    native_menu = false,
+  },
+  formatting = {
+    max_width = 0,
+    duplicates = {
+      buffer = 1,
+      path = 1,
+      nvim_lsp = 0,
+      luasnip = 1,
+    },
+    duplicates_default = 0,
+    fields = { "kind", "abbr" ,"menu" },
+    format = function(entry, vim_item, ki, kk)
+      vim_item.ki = kind_icons[vim_item.kind]
+      vim_item.kk = vim_item.kind
+      vim_item.kind = string.format('%s', vim_item.ki)
+      vim_item.menu = string.format("%s %s", vim_item.kk, source_names[entry.source.name])
+      return vim_item
+    end
+  },
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "luasnip" },
+    { name = "cmp_tabnine" },
+    { name = "nvim_lua" },
+    { name = "buffer" },
+    { name = "calc" },
+    { name = "emoji" },
+    { name = "treesitter" },
+    { name = "crates" },
+    { name = "tmux" },
+  },
+  mapping = {
+    ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert , exper = true}),
+    ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert , exper = true}),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+  },
+}
+
+cmp.setup(config)
