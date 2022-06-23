a362126f61984fdfa987f3ef602a64471e699b8f
sameet <sameetmoosavi@yahoo.com>
2022-06-23 12:45:34 +0430

    config active or diactive extensions

--- lua/vsn/extensions.lua
+++ lua/vsn/extensions.lua
@@ -8,58 +8,58 @@ return packer.startup(function(use)
   -- onedarker
   use { "vsneovim/onedarker.nvim" }
   -- icons
-  use { "kyazdani42/nvim-web-devicons" }
+  use { "kyazdani42/nvim-web-devicons", disable = not is_enabled("useIcon") }
   -- alpha
   use { "goolord/alpha-nvim" }
   -- nvim-tree
-  use { "kyazdani42/nvim-tree.lua" }
+  use { "kyazdani42/nvim-tree.lua", disable = not is_enabled("nvimTree") }
   -- autopairs
   use { "windwp/nvim-autopairs" }
   -- lspconfig
-  use { "neovim/nvim-lspconfig" }
-  use { "williamboman/nvim-lsp-installer" }
-  use { "glepnir/lspsaga.nvim" }
-  use { "jose-elias-alvarez/null-ls.nvim" }
+  use { "neovim/nvim-lspconfig", disable = not is_enabled("LSP") }
+  use { "williamboman/nvim-lsp-installer", disable = not is_enabled("LSP") }
+  use { "glepnir/lspsaga.nvim", disable = not is_enabled("LSP") }
+  use { "jose-elias-alvarez/null-ls.nvim", disable = not is_enabled("LSP") }
   -- cmp
-  use { "hrsh7th/cmp-nvim-lsp" }
-  use { "hrsh7th/cmp-buffer" }
-  use { "hrsh7th/cmp-path" }
-  use { "hrsh7th/cmp-cmdline" }
-  use { "hrsh7th/nvim-cmp" }
-  use { "L3MON4D3/LuaSnip" }
-  use { "saadparwaiz1/cmp_luasnip" }
+  use { "hrsh7th/cmp-nvim-lsp", disable = not is_enabled("CMP") }
+  use { "hrsh7th/cmp-buffer", disable = not is_enabled("CMP") }
+  use { "hrsh7th/cmp-path", disable = not is_enabled("CMP") }
+  use { "hrsh7th/cmp-cmdline", disable = not is_enabled("CMP") }
+  use { "hrsh7th/nvim-cmp", disable = not is_enabled("CMP") }
+  use { "L3MON4D3/LuaSnip", disable = not is_enabled("CMP") }
+  use { "saadparwaiz1/cmp_luasnip", disable = not is_enabled("CMP") }
   -- popup
   use { "nvim-lua/popup.nvim" }
   -- plenary
   use { "nvim-lua/plenary.nvim" }
   -- syntax highlighting (treesitter)
-  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate", disable = not is_enabled("Treesitter") }
   -- fix cursor hold
   use { "antoinemadec/FixCursorHold.nvim" }
   -- notifications (notify)
   use { "rcarriga/nvim-notify" }
-  use { "nvim-telescope/telescope.nvim" }
+  use { "nvim-telescope/telescope.nvim", disable = not is_enabled("Telescope") }
   -- comment
   use { "JoosepAlviste/nvim-ts-context-commentstring" }
   use { "numToStr/Comment.nvim" }
   -- git
-  use { "lewis6991/gitsigns.nvim" }
+  use { "lewis6991/gitsigns.nvim", disable = not is_enabled("Git") }
   -- which-key
-  use { "max397574/which-key.nvim" }
+  use { "max397574/which-key.nvim", disable = not is_enabled("WhichKey") }
   -- project
   use { "ahmedkhalf/project.nvim" }
   -- bufferline
-  use { "akinsho/bufferline.nvim" }
+  use { "akinsho/bufferline.nvim", disable = not is_enabled("BufferLine") }
   -- statusline (bufferline)
   use { "nvim-lualine/lualine.nvim" }
   -- terminal
-  use { "akinsho/toggleterm.nvim" }
+  use { "akinsho/toggleterm.nvim", disable = not is_enabled("Terminal") }
   -- tagbar
   use { "simrat39/symbols-outline.nvim" }
   -- debuging
-  use { "rcarriga/nvim-dap-ui" }
-  use { "mfussenegger/nvim-dap" }
-  use { "ttbug/DAPInstall.nvim" }
+  use { "rcarriga/nvim-dap-ui", disable = not is_enabled("DAP") }
+  use { "mfussenegger/nvim-dap", disable = not is_enabled("DAP") }
+  use { "ttbug/DAPInstall.nvim", disable = not is_enabled("DAP") }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
