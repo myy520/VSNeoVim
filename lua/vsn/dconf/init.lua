07f8e4ac59cee6d82b6412e66726f91ff177204d
sameet <sameetmoosavi@yahoo.com>
2022-06-27 20:09:07 +0430

    update dconf/init.lua

--- lua/vsn/dconf/init.lua
+++ lua/vsn/dconf/init.lua
@@ -1,13 +1,11 @@
 local C = {}
 
 function C:load()
-  local utils = require "vsn.utils"
-
   require "vsn.dconf.defaultConfig"
 
   vim.cmd('luafile ' .. vim.fn.stdpath("config") .. "/config.lua")
 
-  utils.require_clean("vsn.dconf.settings").load_options()
+  require("vsn.dconf.settings").load_options()
 
   require("vsn.dconf.core"):load()
 end
