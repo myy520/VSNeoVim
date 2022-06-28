a6d4532b0f2438c7e4d57bac3f67e09c76df00ac
sameet <sameetmoosavi@yahoo.com>
2022-06-28 19:46:34 +0430

    update lspinstaller

--- lua/vsn/lsp/lsp-installer.lua
+++ lua/vsn/lsp/lsp-installer.lua
@@ -1,58 +1,36 @@
-local utils = require "vsn.utils"
+local utils = require("vsn.utils")
 
-local lsp_installer = utils.require_clean "nvim-lsp-installer"
+local LSPI = utils.require_clean("nvim-lsp-installer")
 
-local lsp_config = utils.require_clean "lspconfig"
+local LSPC = utils.require_clean("lspconfig")
 
-local servers = {
-  "cssls",
-  "html",
-  "sumneko_lua",
-  "tsserver",
-  "pyright",
-}
-
-local settings = {
-  ensure_installed = servers,
+local config = {
+  automatic_installation = true,
   ui = {
-    icons = {
-      server_installed = " ",
-      server_pending = "➜ ",
-      server_uninstalled = "",
-    },
-    keymaps = {
-      toggle_server_expand = "<CR>",
-      install_server = "i",
-      update_server = "u",
-      check_server_version = "c",
-      update_all_servers = "U",
-      check_outdated_servers = "C",
-      uninstall_server = "X",
+    -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+    border = "rounded",
+    ui = {
+      icons = {
+        server_installed = " ",
+        server_pending = "➜ ",
+        server_uninstalled = "",
+      },
+      keymaps = {
+        toggle_server_expand = "<CR>",
+        install_server = "i",
+        update_server = "u",
+        check_server_version = "c",
+        update_all_servers = "U",
+        check_outdated_servers = "C",
+        uninstall_server = "X",
+      },
     },
   },
-
   log_level = vim.log.levels.INFO,
 }
 
-lsp_installer.setup(settings)
+LSPI.setup(config)
 
-local opts = {}
-
-for _, server in pairs(servers) do
-  opts = {
-    on_attach = require("vsn.lsp.handlers").on_attach,
-    capabilities = require("vsn.lsp.handlers").capabilities,
-  }
-
-  if server == "sumneko_lua" then
-    local sumneko_opts = require "vsn.lsp.settings.sumneko_lua"
-    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-  end
-
-  if server == "pyright" then
-    local pyright_opts = require "vsn.lsp.settings.pyright"
-    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-  end
-
-  lsp_config[server].setup(opts)
+for _, server in ipairs(LSPI.get_installed_servers()) do
+  LSPC[server.name].setup {}
 end
