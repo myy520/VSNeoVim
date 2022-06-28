a4a1b1027b57695a4c4bbecf13455a9b13589331
sameet <sameetmoosavi@yahoo.com>
2022-06-28 19:46:24 +0430

    update lsp/init.lua

--- lua/vsn/lsp/init.lua
+++ lua/vsn/lsp/init.lua
@@ -1,9 +1,9 @@
 local C = {}
 
 function C:init()
+  require("vsn.lsp.handlers").setup()
   require "vsn.lsp.lsp-saga"
   require "vsn.lsp.lsp-installer"
-  require("vsn.lsp.handlers").setup()
   require "vsn.lsp.null-ls"
 end
 
