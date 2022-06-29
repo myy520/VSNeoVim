c6e5cfe26901d0bad4bf6f7cd3f1c3971ac576ea
sameet <sameetmoosavi@yahoo.com>
2022-06-29 12:01:59 +0430

    update lspinstaller configuration

--- lua/vsn/lsp/lsp-installer.lua
+++ lua/vsn/lsp/lsp-installer.lua
@@ -4,32 +4,28 @@ local LSPI = utils.require_clean("nvim-lsp-installer")
 
 local LSPC = utils.require_clean("lspconfig")
 
-local config = {
+LSPI.setup {
   automatic_installation = true,
   ui = {
     -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
     border = "rounded",
-    ui = {
-      icons = {
-        server_installed = " ",
-        server_pending = "➜ ",
-        server_uninstalled = "",
-      },
-      keymaps = {
-        toggle_server_expand = "<CR>",
-        install_server = "i",
-        update_server = "u",
-        check_server_version = "c",
-        update_all_servers = "U",
-        check_outdated_servers = "C",
-        uninstall_server = "X",
-      },
+    icons = {
+      server_installed = " ",
+      server_pending = "➜ ",
+      server_uninstalled = "",
+    },
+    keymaps = {
+      toggle_server_expand = "<CR>",
+      install_server = "i",
+      update_server = "u",
+      check_server_version = "c",
+      update_all_servers = "U",
+      check_outdated_servers = "C",
+      uninstall_server = "X",
     },
   },
   log_level = vim.log.levels.INFO,
 }
-
-LSPI.setup(config)
 
 for _, server in ipairs(LSPI.get_installed_servers()) do
   LSPC[server.name].setup {}
